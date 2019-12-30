a88ca863a78db5dd663120a6f07357f5c6e11d87
ssrlive <ssrlivebox@gmail.com>
2019-12-30 15:06:21 +0800

    PostMessage

--- Source/winMd5Sum.c
+++ Source/winMd5Sum.c
@@ -17,167 +17,166 @@ const TCHAR bad_msg[] =_T("MD5 Check Sums are different.");
 
 BOOL CALLBACK MainDialogFunc(HWND hwndDlg,UINT uMsg,WPARAM wParam,LPARAM lParam)
 {
-	switch(uMsg) {
+    switch(uMsg) {
 
-		case WM_INITDIALOG:
-			{
-				if(lstrlen(gCmdLine)) { // if there is a command line
-					SetDlgItemText(hwndDlg,IDC_FILENAME,gCmdLine);  // set it as the file
-					SendMessage(hwndDlg,WM_COMMAND,ID_CALCULATE,0); // and try to process it
-				}
+    case WM_INITDIALOG:
+        {
+            // make sure the right icon is on the window
+            SendMessage(hwndDlg,WM_SETICON,ICON_SMALL,(LPARAM)LoadIcon(gInstance,MAKEINTRESOURCE(IDI_ICON1)));
+            SetFocus(hwndDlg); // get up front
 
-				// make sure the right icon is on the window
-				SendMessage(hwndDlg,WM_SETICON,ICON_SMALL,(LPARAM)LoadIcon(gInstance,MAKEINTRESOURCE(IDI_ICON1)));
-				SetFocus(hwndDlg); // get up front
+            SendMessage(GetDlgItem(hwndDlg,IDC_MD5COMPARE),EM_LIMITTEXT,32,0);
 
-				SendMessage(GetDlgItem(hwndDlg,IDC_MD5COMPARE),EM_LIMITTEXT,32,0);
-			}
-			return TRUE;
+            if(lstrlen(gCmdLine)) { // if there is a command line
+                SetDlgItemText(hwndDlg,IDC_FILENAME,gCmdLine);  // set it as the file
+                PostMessage(hwndDlg, WM_COMMAND, ID_CALCULATE, 0); // and try to process it
+            }
+        }
+        return TRUE;
 
-		case WM_CLOSE:
-			EndDialog(hwndDlg,1);
-			break;
+    case WM_CLOSE:
+        EndDialog(hwndDlg,1);
+        break;
 
-		case WM_DROPFILES:
-			{
-				TCHAR filename[MAX_PATH]=_T("");
-				HDROP hDrop = (HDROP) wParam;
-				DragQueryFile(hDrop,0,filename,MAX_PATH); // get the file name
-				DragFinish(hDrop);                      // close the handle
-				SetDlgItemText(hwndDlg,IDC_FILENAME,filename);
-				SendMessage(hwndDlg,WM_COMMAND,ID_CALCULATE,0);
-			}
-			break;
+    case WM_DROPFILES:
+        {
+            TCHAR filename[MAX_PATH]=_T("");
+            HDROP hDrop = (HDROP) wParam;
+            DragQueryFile(hDrop,0,filename,MAX_PATH); // get the file name
+            DragFinish(hDrop);                      // close the handle
+            SetDlgItemText(hwndDlg,IDC_FILENAME,filename);
+            PostMessage(hwndDlg,WM_COMMAND,ID_CALCULATE,0);
+        }
+        break;
 
-		case WM_COMMAND:
-			switch(LOWORD(wParam)) {
-				case IDOK:
-					EndDialog(hwndDlg,1);
-					break;
+    case WM_COMMAND:
+        switch(LOWORD(wParam)) {
+        case IDOK:
+            EndDialog(hwndDlg,1);
+            break;
 
-				case IDC_FILENAME:
-					if(HIWORD(wParam)==EN_CHANGE) {
-						SetDlgItemText(hwndDlg,IDC_MD5SUM,_T(""));
-					}
-					break;
+        case IDC_FILENAME:
+            if(HIWORD(wParam)==EN_CHANGE) {
+                SetDlgItemText(hwndDlg,IDC_MD5SUM,_T(""));
+            }
+            break;
 
-				case IDC_COMPARE:
-					{
-						TCHAR md5sum[33]=_T(""),md5sumcompare[33]=_T("");
+        case IDC_COMPARE:
+            {
+                TCHAR md5sum[33]=_T(""),md5sumcompare[33]=_T("");
 
-						GetDlgItemText(hwndDlg,IDC_MD5SUM,md5sum,MAX_PATH);
-						GetDlgItemText(hwndDlg,IDC_MD5COMPARE,md5sumcompare,MAX_PATH);
+                GetDlgItemText(hwndDlg,IDC_MD5SUM,md5sum,MAX_PATH);
+                GetDlgItemText(hwndDlg,IDC_MD5COMPARE,md5sumcompare,MAX_PATH);
 
-						if(lstrcmp(md5sum,md5sumcompare)) {
-							// failed
-							MessageBox(hwndDlg,bad_msg,_T(""),MB_OK|MB_ICONERROR);
-						} else {
-							// success
-							MessageBox(hwndDlg,good_msg,_T(""),MB_OK|MB_ICONINFORMATION);
-						}
+                if(lstrcmp(md5sum,md5sumcompare)) {
+                    // failed
+                    MessageBox(hwndDlg,bad_msg,_T(""),MB_OK|MB_ICONERROR);
+                } else {
+                    // success
+                    MessageBox(hwndDlg,good_msg,_T(""),MB_OK|MB_ICONINFORMATION);
+                }
 
-					}
-					break;
+            }
+            break;
 
-				case IDC_BROWSE:
-					{
-						TCHAR filename[MAX_PATH]=_T("");
-						TCHAR mask[]=_T("*.*\0\0");
-						OPENFILENAME open;
-  
-						ZeroMemory(&open,sizeof(OPENFILENAME));
-						open.lStructSize  =(DWORD)sizeof(OPENFILENAME);
-						open.lpstrFilter  =mask;
-						open.nFilterIndex =0;
-						open.lpstrFile    =filename;
-						open.nMaxFile     =MAX_PATH;
-						open.Flags        =OFN_EXPLORER;
-						open.lpstrDefExt  =_T("");
-						open.hwndOwner    =hwndDlg;
-						open.hInstance    =gInstance;
+        case IDC_BROWSE:
+            {
+                TCHAR filename[MAX_PATH]=_T("");
+                TCHAR mask[]=_T("*.*\0\0");
+                OPENFILENAME open;
 
-						if(GetOpenFileName(&open)) {
-							SetDlgItemText(hwndDlg,IDC_FILENAME,filename);
-							SendMessage(hwndDlg,WM_COMMAND,ID_CALCULATE,0);
-						}
+                ZeroMemory(&open,sizeof(OPENFILENAME));
+                open.lStructSize  =(DWORD)sizeof(OPENFILENAME);
+                open.lpstrFilter  =mask;
+                open.nFilterIndex =0;
+                open.lpstrFile    =filename;
+                open.nMaxFile     =MAX_PATH;
+                open.Flags        =OFN_EXPLORER;
+                open.lpstrDefExt  =_T("");
+                open.hwndOwner    =hwndDlg;
+                open.hInstance    =gInstance;
 
-					}
-					break;
+                if(GetOpenFileName(&open)) {
+                    SetDlgItemText(hwndDlg,IDC_FILENAME,filename);
+                    PostMessage(hwndDlg,WM_COMMAND,ID_CALCULATE,0);
+                }
 
-				case ID_CALCULATE:
-					{
-						int i,len;
-						md5_byte_t buffer[512];
-						TCHAR hex[33]=_T("");
-						TCHAR filename[MAX_PATH]=_T("");
-						md5_state_t state;
-						md5_byte_t digest[16];
-						FILE *pfile;
-						
-						GetDlgItemText(hwndDlg,IDC_FILENAME,filename,MAX_PATH);
+            }
+            break;
 
-						if(lstrlen(filename)&&(pfile=_tfopen(filename,_T("rb")))) {
-						
-							md5_init(&state);
-							
-							do {
-								len=fread(buffer,1,512,pfile);
-								if(!ferror(pfile)) md5_append(&state,(const md5_byte_t *)buffer,len);
-								Sleep(0);
-							} while(!feof(pfile));
-							
-							md5_finish(&state, digest);
+        case ID_CALCULATE:
+            {
+                int i,len;
+                md5_byte_t buffer[512];
+                TCHAR hex[33]=_T("");
+                TCHAR filename[MAX_PATH]=_T("");
+                md5_state_t state;
+                md5_byte_t digest[16];
+                FILE *pfile;
 
-							hex[0]=0;
-							for(i=0;i<16;i++) _stprintf(hex,_T("%s%02x"),hex,digest[i]);
+                GetDlgItemText(hwndDlg,IDC_FILENAME,filename,MAX_PATH);
 
-							fclose(pfile);
+                if(lstrlen(filename)&&(pfile=_tfopen(filename,_T("rb")))) {
 
-							SetDlgItemText(hwndDlg,IDC_MD5SUM,hex);
-						} else {
-							SetDlgItemText(hwndDlg,IDC_MD5SUM,_T("Invalid File"));
-						}
-					}
-					break;
+                    md5_init(&state);
 
-				case IDC_ABOUT:
-					ShellExecute(0,_T("open"),_T("http://www.nullriver.com"),0,0,SW_SHOWNORMAL);
-					break;
-			}
-	}
+                    do {
+                        len=fread(buffer,1,512,pfile);
+                        if(!ferror(pfile)) md5_append(&state,(const md5_byte_t *)buffer,len);
+                        Sleep(0);
+                    } while(!feof(pfile));
 
-	return FALSE;
+                    md5_finish(&state, digest);
+
+                    hex[0]=0;
+                    for(i=0;i<16;i++) _stprintf(hex,_T("%s%02x"),hex,digest[i]);
+
+                    fclose(pfile);
+
+                    SetDlgItemText(hwndDlg,IDC_MD5SUM,hex);
+                } else {
+                    SetDlgItemText(hwndDlg,IDC_MD5SUM,_T("Invalid File"));
+                }
+            }
+            break;
+
+        case IDC_ABOUT:
+            ShellExecute(0,_T("open"),_T("http://www.nullriver.com"),0,0,SW_SHOWNORMAL);
+            break;
+        }
+    }
+
+    return FALSE;
 };
 
 void GetParam()
 {
-	int si=0,di=0;
+    int si=0,di=0;
 
-	TCHAR *cmdLine=GetCommandLine();
-	TCHAR exeName[MAX_PATH];
+    TCHAR *cmdLine=GetCommandLine();
+    TCHAR exeName[MAX_PATH];
 
-	GetModuleFileName(gInstance,exeName,MAX_PATH);
+    GetModuleFileName(gInstance,exeName,MAX_PATH);
 
-	// skip past the module name
-	if(_tcsstr(cmdLine,exeName)) {
-		if(cmdLine[0]=='"'&&exeName[0]!='"') si=1;
-		si+=lstrlen(exeName);
-		while(isspace(cmdLine[si])||cmdLine[si]=='"') si++;
-	}
+    // skip past the module name
+    if(_tcsstr(cmdLine,exeName)) {
+        if(cmdLine[0]=='"'&&exeName[0]!='"') si=1;
+        si+=lstrlen(exeName);
+        while(isspace(cmdLine[si])||cmdLine[si]=='"') si++;
+    }
 
-	if(si<lstrlen(cmdLine)) {
-		while(cmdLine[si]) {
-			if(cmdLine[si]!='"') gCmdLine[di++]=cmdLine[si];
-			si++;
-		}
-	}
-	gCmdLine[di]=0;
+    if(si<lstrlen(cmdLine)) {
+        while(cmdLine[si]) {
+            if(cmdLine[si]!='"') gCmdLine[di++]=cmdLine[si];
+            si++;
+        }
+    }
+    gCmdLine[di]=0;
 };
 
-int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLineA,int nCmdShow)
-{
-	gInstance=hInstance;
-	GetParam();
-	InitCommonControls();
-	return DialogBox(hInstance,MAKEINTRESOURCE(IDD_MAIN),0,(DLGPROC)MainDialogFunc);
+int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLineA,int nCmdShow) {
+    gInstance=hInstance;
+    GetParam();
+    InitCommonControls();
+    return DialogBox(hInstance,MAKEINTRESOURCE(IDD_MAIN),0,(DLGPROC)MainDialogFunc);
 };
